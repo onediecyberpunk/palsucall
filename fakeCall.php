458148de8b602df9c5bc5e940a795fd6cacb4de5
onediecyberpunk <36159296+onediecyberpunk@users.noreply.github.com>
2019-05-03 11:35:06 +0700

    Update fakeCall.php

--- fakeCall.php
+++ fakeCall.php
@@ -60,7 +60,7 @@ class prankCall{
 
 		$ch = curl_init();
 
-		curl_setopt($ch, CURLOPT_URL, "https://api.grab.com/grabid/v1/phone/otp");
+		curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");
 
 		curl_setopt($ch, CURLOPT_POST, 1);
 
